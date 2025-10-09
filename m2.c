2d0a39f331e72817c4a9ba63898bc2cd75e5e774
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-09 22:28:57 +0530

    Greatest of three numbers (nested if)

---
+++ m2.c
@@ -0,0 +1,15 @@
+/* 2. Greatest of three numbers (nested if) */
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter 3 nums: ");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>=b){
+        if(a>=c) printf("%d is greatest\n",a);
+        else printf("%d is greatest\n",c);
+    } else {
+        if(b>=c) printf("%d is greatest\n",b);
+        else printf("%d is greatest\n",c);
+    }
+    return 0;
+}
