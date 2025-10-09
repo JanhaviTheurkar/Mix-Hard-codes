ef9929d4c3b54cadb9a781b1bc580f8dab6c83f6
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-09 22:28:17 +0530

    Leap year (nested if)

---
+++ m1.c
@@ -0,0 +1,14 @@
+/* 1. Leap year (nested if) */
+#include <stdio.h>
+int main() {
+    int y;
+    printf("Enter year: ");
+    scanf("%d",&y);
+    if(y%4==0){
+        if(y%100==0){
+            if(y%400==0) printf("%d is a leap year\n",y);
+            else printf("%d is not a leap year\n",y);
+        } else printf("%d is a leap year\n",y);
+    } else printf("%d is not a leap year\n",y);
+    return 0;
+}
