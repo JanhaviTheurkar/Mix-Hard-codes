07a0d46437ba16921b13d9a29e65e7a0ce33564b
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-11 22:25:03 +0530

    Floyd's triangle

---
+++ m24.c
@@ -0,0 +1,10 @@
+/* 24. Floyd's triangle */
+#include <stdio.h>
+int main(){
+    int n,c=1; printf("Rows: "); scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++) printf("%d ", c++);
+        printf("\n");
+    }
+    return 0;
+}
